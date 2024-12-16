94e183e94258ead40ebf535f2b178e8b6cb1842f
Rafael Milewski <rafael.milewski@gmail.com>
2024-12-16 15:34:30 +0800

    add missing controller

---
+++ backend/app/Http/Controllers/StoreAssetsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAssetsRequest;
+use App\Services\BookService;
+
+class StoreAssetsController extends Controller
+{
+    public function __invoke(StoreAssetsRequest $request): bool
+    {
+        if ($request->header('x-api-key') !== config('app.worker_api_key')) {
+            return false;
+        }
+
+        return BookService::resolve()->storeAssets($request);
+    }
+}
