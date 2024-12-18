6758c0ef048c18e96b4fc1ea3523f85e97b9cafb
hello-liang-shan <hello.liang.shan@gmail.com>
2024-12-18 18:47:20 +0800

    add command to upload exising assets to s3

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,3 +1,35 @@
 <?php
 
 declare(strict_types = 1);
+
+use App\Enums\BookState;
+use App\Models\Book;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Artisan;
+
+Artisan::command('book:assets:s3', function () {
+
+    $s3 = Storage::disk('s3');
+    $public = Storage::disk('public');
+
+    Book::query()
+        ->where('state', BookState::Completed->value)
+        ->chunk(100, function (Collection $books) use ($s3, $public) {
+
+            $books->each(function (Book $book) use ($s3, $public) {
+
+                foreach ($book->assets as $asset) {
+
+                    if ($public->fileExists($asset)) {
+
+                        $s3->put($asset, $public->get($asset));
+                        $public->delete($asset);
+
+                    }
+
+                }
+
+            });
+
+        });
+});
