d764aad9b048feefc4c134c8822c70f23c5b4491
milewski <rafael.milewski@gmail.com>
2024-12-21 22:12:10 +0800

    finish backend for the tts feature

--- backend/app/Http/Controllers/StoreSpeechController.php
+++ backend/app/Http/Controllers/StoreSpeechController.php
@@ -4,7 +4,6 @@ declare(strict_types = 1);
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\StoreAssetsRequest;
 use App\Http\Requests\StoreSpeechRequest;
 use App\Models\Book;
 use App\Services\BookService;
--- backend/app/Http/Requests/StoreAssetsRequest.php
+++ backend/app/Http/Requests/StoreAssetsRequest.php
@@ -4,9 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Http\Requests;
 
-use App\Models\Book;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class StoreAssetsRequest extends FormRequest
 {
