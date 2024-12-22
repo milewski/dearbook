b85798260eb65f7ca6dc2692c56adae4bdf5c888
milewski <rafael.milewski@gmail.com>
2024-12-22 09:15:17 +0800

    remove test code

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -1,21 +1,3 @@
 <?php
 
 declare(strict_types = 1);
-
-
-use Illuminate\Redis\Connections\PhpRedisConnection;
-
-Artisan::command('demo', function () {
-
-    /**
-     * @var PhpRedisConnection $redis
-     */
-    $redis = \Illuminate\Support\Facades\Redis::connection();
-
-    $redis->setnx("c68b0525-b482-480a-984d-13929308fe17", "yeah");
-
-//    $redis->publish('channel_2', json_encode([
-//        'name' => 'Adam Wathan',
-//    ]));
-
-});
